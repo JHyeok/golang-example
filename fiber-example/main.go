00aad33f2e7f922f310ec6a1790496a4b3f7a5fc
JaeHyeok Kim <dev.jhyeok@gmail.com>
2022-02-27 23:42:45 +0900

    Fiber v2 Basic routing, Static files

--- fiber-example/main.go
+++ fiber-example/main.go
@@ -5,6 +5,12 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	app.Static("/", "./public")
+
+	app.Get("/:value", func(c *fiber.Ctx) error {
+		return c.SendString("value: " + c.Params("value"))
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
